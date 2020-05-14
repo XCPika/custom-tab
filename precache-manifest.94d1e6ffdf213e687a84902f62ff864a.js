9aa54564bd4ff901d50c081adcbff9094c28f73f
XCPika <themightmage@gmail.com>
2020-05-14 11:17:34 +0100

    Updates

---
+++ precache-manifest.94d1e6ffdf213e687a84902f62ff864a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "929f1fa1ab98c0c5bb7ef21d7f94f014",
+    "url": "/custom-tab/index.html"
+  },
+  {
+    "revision": "582408ee05267cbe5833",
+    "url": "/custom-tab/static/css/main.685dea8f.chunk.css"
+  },
+  {
+    "revision": "399a7cd451eeba7e6fb0",
+    "url": "/custom-tab/static/js/2.cd74b548.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/custom-tab/static/js/2.cd74b548.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "582408ee05267cbe5833",
+    "url": "/custom-tab/static/js/main.2576ac63.chunk.js"
+  },
+  {
+    "revision": "efa76316ffc84a62c4f6",
+    "url": "/custom-tab/static/js/runtime-main.3dfd18de.js"
+  }
+]);
