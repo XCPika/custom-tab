e539943eae74db314288f4ee80786f4aaf085bb3
XCPika <61687817+XCPika@users.noreply.github.com>
2020-05-14 10:51:50 +0100

    Add files via upload

---
+++ precache-manifest.29061a02bc6bb81d690f4faa8ea8615d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb806f5f247c1ac34f818dd7335c77cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c552170797777c95231",
+    "url": "/static/css/main.685dea8f.chunk.css"
+  },
+  {
+    "revision": "4ac0ed1947888d33d9fc",
+    "url": "/static/js/2.e5d6121e.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.e5d6121e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c552170797777c95231",
+    "url": "/static/js/main.ac20f2bc.chunk.js"
+  },
+  {
+    "revision": "522850aa382f2ff37b84",
+    "url": "/static/js/runtime-main.cd552139.js"
+  }
+]);
